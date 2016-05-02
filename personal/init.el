34c421320f62b2b8b86348adf2dfd627b1168ae5
Jeremy Hetzel <jthetzel@gmail.com>
2016-05-02 11:32:01 -0230

    Enable web-mode and js2-jsx-mode file types.

--- personal/init.el
+++ personal/init.el
@@ -86,3 +86,16 @@
 ;; es-mode for Elasticsearch
 (add-to-list 'auto-mode-alist '("\\.es$" . es-mode))
 
+;; web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; js2-jsx-mode
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-jsx-mode))
