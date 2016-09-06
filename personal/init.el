70a6939ecd85b4934e24b9e0789db8797e755f53
Jeremy Hetzel <jthetzel@gmail.com>
2016-09-06 13:26:27 -0230

    js2mode: ignore trailing comma

--- personal/init.el
+++ personal/init.el
@@ -122,7 +122,8 @@
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)
             (setq-default tab-width 2)
-            (setq js2-strict-missing-semi-warning nil)))
+            (setq js2-strict-missing-semi-warning nil)
+            (setq js2-strict-trailing-comma-warning nil)))
 
 ;; magit-gitflow
 ;;; C-f in the magit status buffer invokes the magit-gitflow popup. If you
